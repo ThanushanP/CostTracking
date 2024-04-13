b126713ba152c6e1cbe54827b11b9c5d15833c09
Thanushan <thanushanpirapakaran@gmail.com>
2024-04-13 18:30:34 -0400

    Scrolling works, calender filtering

--- app/src/main/java/ca/brocku/costtracking/Spenditure.java
+++ app/src/main/java/ca/brocku/costtracking/Spenditure.java
@@ -2,6 +2,9 @@ package ca.brocku.costtracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,12 +15,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-public class Spenditure extends AppCompatActivity {
+public class Spenditure extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +34,43 @@ public class Spenditure extends AppCompatActivity {
         setContentView(R.layout.activity_spenditure);
 
         getSupportActionBar().setTitle("Spenditures");
-        query();
+        query("L", false);
 
     }
 
-    private void query() {
+    private void query(String date, boolean useDate) {
         String[] fields=new String[]{"rule","userName","date","amount","LocationName","time"};
         ListView listView = findViewById(R.id.Spendingslist);
+        TextView textView = findViewById(R.id.totalcost);
+        int total = 0;
+        String selection;
+        String[] selectionArgs;
 
         ArrayList<String> entries=new ArrayList<>();
 
         DataHelper dh=new DataHelper(this);
         SQLiteDatabase datareader=dh.getReadableDatabase();
-
-        String selection = "userName = ?";
-        String[] selectionArgs = { "UserName" };//Change this for the name or email of the user
-
-
+        if (!useDate){
+            selection = "userName = ?";
+            selectionArgs = new String[]{"UserName"};//Change this for the name or email of the user
+        }
+        else{
+            selection = "userName = ? AND date = ?";
+            selectionArgs = new String[]{"UserName", date};//Change this for the name or email of the user
+        }
         Cursor cursor=datareader.query(DataHelper.DB_TABLE,fields,selection,selectionArgs,null,null,"date DESC, time DESC");
+
         cursor.moveToFirst();
 
         while (!cursor.isAfterLast()) {
+            total+= Integer.parseInt(cursor.getString(3));
             entries.add(cursor.getString(4)+"\n$"+cursor.getString(3)+"\n"+cursor.getString(2)+" | "+cursor.getString(5));
             cursor.moveToNext();
         }
 
         if (!cursor.isClosed()) cursor.close();
+
+        textView.setText("Total: $"+total);
         CustomCursorAdapter adapter = new CustomCursorAdapter(this, entries);
         listView.setAdapter(adapter);
 
@@ -88,5 +108,29 @@ public class Spenditure extends AppCompatActivity {
 
     public void toSpend(View view) {
         startActivity(new Intent(this, Spenditure.class));
+    }
+
+    public void changeDate(View view) {
+        Calendar today=Calendar.getInstance();
+        new DatePickerDialog(
+                this,
+                this,
+                today.get(Calendar.YEAR),//year
+                today.get(Calendar.MONTH),//month
+                today.get(Calendar.DAY_OF_MONTH)//day
+        ).show();
+    }
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar today=Calendar.getInstance();
+        today.set(Calendar.YEAR, year);
+        today.set(Calendar.MONTH, monthOfYear);
+        today.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        Date selectedDate = today.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String selectedDateText = dateFormat.format(selectedDate);
+
+        query(selectedDateText,true);
     }
 }
--- app/src/main/java/ca/brocku/costtracking/inputSpend.java
+++ app/src/main/java/ca/brocku/costtracking/inputSpend.java
@@ -8,6 +8,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
@@ -79,10 +80,18 @@ public class inputSpend extends AppCompatActivity implements DatePickerDialog.On
         DataHelper dh=new DataHelper(this);
         SQLiteDatabase datachanger=dh.getWritableDatabase();
 
+        Editable text = amountWidget.getText();
+        String amount;
+        if (text.length() == 0) {
+            amount = "0";
+        } else {
+            amount = amountWidget.getText().toString();
+        }
+
         ContentValues newWisdom=new ContentValues();
         newWisdom.put("userName","UserName");//repalce with username
         newWisdom.put("locationName",nameWidget.getText().toString());
-        newWisdom.put("amount",amountWidget.getText().toString());
+        newWisdom.put("amount",amount);
         newWisdom.put("date",selectedDateText);
         newWisdom.put("time",selectedTimeText);
 
