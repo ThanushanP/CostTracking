dbf88a247ed4000c61498657513d0d12542ba475
Adrian <adrianmbinu@gmail.com>
2024-04-17 16:23:03 -0400

    uhhh idk what to put here fdfd

--- app/src/main/java/ca/brocku/costtracking/GasPrices.java
+++ app/src/main/java/ca/brocku/costtracking/GasPrices.java
@@ -95,7 +95,7 @@ public class GasPrices extends AppCompatActivity {
     }
 
     private String getGasPrices(String zipCode) {
-        String baseUrl = "http://192.168.0.108:5000//gas_prices?zip_code=";
+        String baseUrl = "https://adrian.tpgc.me:443//gas_prices?zip_code=";
         String apiUrl = baseUrl + zipCode;
         try {
             URL url = new URL(apiUrl);
